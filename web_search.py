1e25d339a32ee9e2943be94cf98c31bbb4250153
andreytata <andreytata@ex.ua>
2019-12-22 15:49:19 +0200

    multiple engines used in search

--- web_search.py
+++ web_search.py
@@ -12,10 +12,31 @@ black_list=\
 , u"https://animego"              : u"тест черного списка"
 }
 
+def find_use_yahoo_no_js( query ):
+    """Serch Web use "Yahoo". Return sorted urls list w/o dublicates"""
+    query = urllib.parse.quote(query)  # comment this line to crash test 2 in unittests 
+    query = "https://search.yahoo.com/search?p=%s&ei=UTF-8&nojs=1" % query
+    buff  = str(urllib.request.urlopen(query).read())
+    bs    = bs4.BeautifulSoup( buff, "html.parser" )
+    links = [ i for i in bs.find_all('a') if not ".yahoo.com" in repr(i) ]
+    links = [ re.findall("href=\"([^\"]*)\"", str(i)) for i in links ]
+    links = [ urllib.parse.unquote( i[0] ) for i in links if i ]
+    links = [ i.replace("https://www.", "https://") for i in links ]
+    links = [ i.replace("http://www.", "http://") for i in links ]
+    clean = [ ]
+    for key in black_list:
+        for link in links:
+            if not link.startswith("http"):
+                print ("REMOVE " + link + "=> AS INVALID LINK" )
+                continue
+            if link.startswith(key):
+                print ("REMOVE " + key + "=>" + black_list[key] )
+                continue
+            clean.append(link)
+    return sorted( [ i for i in set(clean) ] )
+
 def find_use_duck_duck_go( query ):
-    """  Serch Web use "Duck-Duck-Go" serch engine.
-       Return sorted urls list w/o dublicates
-    """
+    """Serch Web use "Duck-Duck-Go". Return sorted urls list w/o dublicates"""
     query = urllib.parse.quote(query)  # comment this line to crash test 2 in unittests 
     query = "https://duckduckgo.com/html/?q=%s" % query
     buff  = str(urllib.request.urlopen(query).read())
@@ -24,7 +45,6 @@ def find_use_duck_duck_go( query ):
     links = [ urllib.parse.unquote( i[0] ) for i in links if i ]
     links = [ i.replace("https://www.", "https://") for i in links ]
     links = [ i.replace("http://www.", "http://") for i in links ]
-    
     clean = [ ]
     for key in black_list:
         for link in links:
@@ -32,18 +52,20 @@ def find_use_duck_duck_go( query ):
                 print ("REMOVE " + key + "=>" + black_list[key] )
                 continue
             clean.append(link)
-
     return sorted( [ i for i in set(clean) ] )
 
 def search(query):
-    return find_use_duck_duck_go( query )
+    yahoo_result = find_use_yahoo_no_js  ( query )
+    duck_duck_go = find_use_duck_duck_go ( query )
+    return sorted( set( yahoo_result + duck_duck_go ) )
 
 if __name__=="__main__":
     query = "+".join( sys.argv[1:] )
     if "" == query:
         sys.exit(0)
+    result = search( query )
     print( sys.prefix, sys.version )
     print( '''Web Search "%s"''' % query )
-    print( "\n".join( [ "%02d %s" % i for i in enumerate( search( query ) ) ] ) )
+    print( "\n".join( [ "%02d %s" % i for i in enumerate( result ) ] ) )
 
     #print("\n".join([ re.findall(r"""<a.+href=\"([^>]*)\">""",str(i))[0] for i in buff.find_all('a') ]))
