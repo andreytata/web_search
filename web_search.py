93f84cc46c2faeacc758c0d547f48568fb4459e8
andreytata <andreytata@ex.ua>
2019-12-26 11:24:34 +0200

    clean removed from plugins

--- web_search.py
+++ web_search.py
@@ -1,6 +1,6 @@
 """ Serch Web use "Duck-Duck-Go" serch engine
 >>> import web_search
->>> urls = web_search.find_use_duck_duck_go("anime+ru"))
+>>> urls = web_search.search("anime+ru"))
 python3 -m web_search naruto boruto online ru 
 """
 
@@ -9,7 +9,7 @@ import re, urllib.request, urllib.parse, bs4, sys, pdb
 black_list=\
 { u"https://smotret-anime.online" : u"платный"
 , u"https://google.com"           : u"повторный поиск, - эхо ключевых слов запроса"
-, u"https://animego"              : u"тест черного списка"
+#, u"https://animego"              : u"тест черного списка"
 }
 
 def find_use_bing_no_js(query):
@@ -23,17 +23,7 @@ def find_use_bing_no_js(query):
     links = [ urllib.parse.unquote( i[0] ) for i in links if i ]
     links = [ i.replace("https://www.", "https://") for i in links ]
     links = [ i.replace("http://www.", "http://") for i in links ]
-    clean = [ ]
-    for key in black_list:
-        for link in links:
-            if not link.startswith("http"):
-                print ("REMOVE " + link + "=> AS INVALID LINK" )
-                continue
-            if link.startswith(key):
-                print ("REMOVE " + key + "=>" + black_list[key] )
-                continue
-            clean.append(link)
-    return sorted( [ i for i in set(clean) ] )
+    return sorted(set(links))
 
 def find_use_yahoo_no_js( query ):
     """Serch Web use "Yahoo". Return sorted urls list w/o dublicates"""
@@ -46,17 +36,7 @@ def find_use_yahoo_no_js( query ):
     links = [ urllib.parse.unquote( i[0] ) for i in links if i ]
     links = [ i.replace("https://www.", "https://") for i in links ]
     links = [ i.replace("http://www.", "http://") for i in links ]
-    clean = [ ]
-    for key in black_list:
-        for link in links:
-            if not link.startswith("http"):
-                print ("REMOVE " + link + "=> AS INVALID LINK" )
-                continue
-            if link.startswith(key):
-                print ("REMOVE " + key + "=>" + black_list[key] )
-                continue
-            clean.append(link)
-    return sorted( [ i for i in set(clean) ] )
+    return sorted(set(links))
 
 def find_use_duck_duck_go( query ):
     """Serch Web use "Duck-Duck-Go". Return sorted urls list w/o dublicates"""
@@ -68,20 +48,24 @@ def find_use_duck_duck_go( query ):
     links = [ urllib.parse.unquote( i[0] ) for i in links if i ]
     links = [ i.replace("https://www.", "https://") for i in links ]
     links = [ i.replace("http://www.", "http://") for i in links ]
-    clean = [ ]
-    for key in black_list:
-        for link in links:
-            if link.startswith(key):
-                print ("REMOVE " + key + "=>" + black_list[key] )
-                continue
-            clean.append(link)
-    return sorted( [ i for i in set(clean) ] )
+    return sorted(set(links))
 
 def search(query):
     yahoo_result = find_use_yahoo_no_js  ( query )
     duck_duck_go = find_use_duck_duck_go ( query )
     bing_results = find_use_bing_no_js   ( query )
-    return sorted( set( yahoo_result + duck_duck_go + bing_results ) )
+    links = yahoo_result + duck_duck_go + bing_results
+    clean = [ ]
+    for key in black_list:
+        for link in links:
+            if not link.startswith("http"):
+                print ("REMOVE " + link + "=> AS INVALID LINK" )
+                continue
+            if link.startswith(key):
+                print ("REMOVE " + key + "=>" + black_list[key] )
+                continue
+            clean.append(link)
+    return sorted( set( clean ) )
 
 if __name__=="__main__":
     query = "+".join( sys.argv[1:] )
